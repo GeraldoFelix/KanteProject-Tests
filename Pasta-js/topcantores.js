bc6749d2f8b932cac9c127dfa2bb0f5baac46261
mimia <yasmom.gamer@gmail.com>
2025-02-19 14:24:51 -0300

    Add files via upload

--- Pasta-js/topcantores.js
+++ Pasta-js/topcantores.js
@@ -3,24 +3,24 @@ async function carregarArtistas() {
         const resposta = await fetch("https://kanteproject-tests.onrender.com/api/artistas");
         if (!resposta.ok) throw new Error("Erro ao buscar os artistas.");
 
-        const artistas = await resposta.json();
-        console.log("Artistas carregados:", artistas); 
+        const dados = await resposta.json();
+        console.log("Artistas carregados:", dados); 
 
-        const container = document.querySelector(".container-imagecantores");
-        container.innerHTML = ""; 
+        const container = document.getElementById("container-cantores");
 
-        if (artistas.length === 0) {
+        if (dados.length === 0) {
             container.innerHTML = "<p>Nenhum artista encontrado.</p>";
             return;
         }
 
-        artistas.forEach(artista => {
+        dados.forEach(artista => {
             const div = document.createElement("div");
             div.classList.add("organizador");
+            div.onclick = () => selecionarArtista(artista.id, artista.name, artista.image);
 
             div.innerHTML = `
                 <img src="../Imagens/Botao_play.png" class="botao-tocar">
-                <img src="${artista.image}" class="imagens" alt="${artista.name}">
+                <img src="${artista.image}" class="imagens" alt="${artista.name})">
                 <p>${artista.name}</p>
             `;
 
@@ -29,6 +29,12 @@ async function carregarArtistas() {
     } catch (erro) {
         console.error("Erro ao carregar artistas:", erro);
     }
+}
+function selecionarArtista(id, name, image) {
+    localStorage.setItem("artistaSelecionado", id);
+    localStorage.setItem("artistaNome", name);
+    localStorage.setItem("artistaImagem", image);
+    window.location.href = "perfil_artista.html";
 }
 
 document.addEventListener("DOMContentLoaded", carregarArtistas);
